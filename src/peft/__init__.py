9f491966093c4e052f8851fb0a20a0e7c144bcc2
qula0496 <qula0496@gpu4-119-5.cs.usyd.edu.au>
2024-03-11 14:55:38 +1100

    add vera to peft

--- src/peft/__init__.py
+++ src/peft/__init__.py
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.9.1.dev0"
+__version__ = "0.9.1.quan"
 
 from .auto import (
     AutoPeftModel,
@@ -73,6 +73,8 @@ from .tuners import (
     OFTModel,
     PolyConfig,
     PolyModel,
+    VeraConfig,
+    VeraModel
 )
 from .utils import (
     TRANSFORMERS_MODELS_TO_PREFIX_TUNING_POSTPROCESS_MAPPING,

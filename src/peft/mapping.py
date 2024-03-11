86914ed957f639eb3e777be75386cceaada8b258
qula0496 <qula0496@gpu4-119-5.cs.usyd.edu.au>
2024-03-11 15:06:11 +1100

    change mapping.py for vera

--- src/peft/mapping.py
+++ src/peft/mapping.py
@@ -49,6 +49,8 @@ from .tuners import (
     PrefixTuningConfig,
     PromptEncoderConfig,
     PromptTuningConfig,
+    VeraModel,
+    VeraConfig
 )
 from .utils import _prepare_prompt_learning_config
 
@@ -79,6 +81,7 @@ PEFT_TYPE_TO_CONFIG_MAPPING: dict[str, PeftConfig] = {
     "MULTITASK_PROMPT_TUNING": MultitaskPromptTuningConfig,
     "OFT": OFTConfig,
     "POLY": PolyConfig,
+    "VERA": VeraConfig
 }
 
 PEFT_TYPE_TO_TUNER_MAPPING = {
@@ -89,6 +92,7 @@ PEFT_TYPE_TO_TUNER_MAPPING = {
     "IA3": IA3Model,
     "OFT": OFTModel,
     "POLY": PolyModel,
+    "VERA": VeraModel
 }
 
 

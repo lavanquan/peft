720a42e3b0ed268f4e0803e798bc179e6a461bc3
qula0496 <qula0496@gpu4-119-5.cs.usyd.edu.au>
2024-03-11 16:30:34 +1100

    update vera model

--- src/peft/tuners/vera/layer.py
+++ src/peft/tuners/vera/layer.py
@@ -52,12 +52,12 @@ class VeraLayer(LoraLayer):
         # Actual trainable parameters
         # Right singular vectors
         std_dev = 1./torch.sqrt(torch.tensor(r).float())
-        self.lora_A[adapter_name] = nn.Parameter(torch.normal((r, self.in_features), mean=0, std=std_dev), requires_grad=False)
+        self.lora_A[adapter_name] = nn.Parameter(torch.normal(size=(r, self.in_features), mean=0, std=std_dev), requires_grad=False)
         # Singular values
         # self.lora_E[adapter_name] = nn.Parameter(torch.randn(r, 1))
         self.lora_d[adapter_name] = nn.Parameter(torch.ones(r, 1))
         # Left singular vectors
-        self.lora_B[adapter_name] = nn.Parameter(torch.normal((self.out_features, r), mean=0, std=std_dev), requires_grad=False)
+        self.lora_B[adapter_name] = nn.Parameter(torch.normal(size=(self.out_features, r), mean=0, std=std_dev), requires_grad=False)
         self.lora_b[adapter_name] = nn.Parameter(torch.zeros(1, r))
         # The current rank
         # self.ranknum[adapter_name] = nn.Parameter(torch.randn(1), requires_grad=False)
